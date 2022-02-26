141c962c649c6e1ceaefaa2d71cce634110bdc91
fawda123 <mbafs2012@gmail.com>
2022-02-26 12:29:52 -0500

    data build ci done

---
+++ R/dat_build.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(here)
+library(googledrive)
+library(googlesheets4)
+
+# deauth all so it can build with gh actions
+drive_deauth()
+gs4_deauth()
+
+datraw <- read_sheet('1TOIquQt2WtM1FpQjc1uKp1GRdbYxen0zD9g78gYcmV8', sheet = 'Raw')
+
+dat <- datraw %>% 
+  mutate(
+    Time = as.character(unlist(Time)), 
+    Date = as.character(Date)
+  ) %>% 
+  unite('DateTime', Date, Time) %>% 
+  mutate(
+    DateTime = ymd_hm(DateTime, tz = 'EST')
+  )
+
+save(dat, file = here('data/dat.RData'))
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # baby-stats
 
+[![build](https://github.com/fawda123/baby-stats/actions/workflows/databuild.yaml/badge.svg)](https://github.com/fawda123/baby-stats/actions/workflows/databuild.yaml)
+
 Materials for evaluation of baby data
